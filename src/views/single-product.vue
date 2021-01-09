a18f2d986b2ad685672959c123dbc0fe05d064d8
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2021-01-09 21:45:22 +0600

    single-product page complete

--- src/views/single-product.vue
+++ src/views/single-product.vue
@@ -8,30 +8,28 @@
           <img src="../assets/men-category.jpg" style="width: 100%" />
         </div>
         <div class="col-5">
-          <div
-            style="width: 75%; display: flex; justify-content: space-between"
-          >
-            <div>Home</div>
+          <div style="width: 80%; display: flex; justify-content: space-between">
+            <div><strong>Home</strong></div>
             <div>
-              <font-awesome-icon :icon="['fas', 'long-arrow-alt-right']" />
+              <font-awesome-icon style="color: lightgray" :icon="['fas', 'long-arrow-alt-right']" />
             </div>
-            <div>Men</div>
+            <div><strong>Men</strong></div>
             <div>
-              <font-awesome-icon :icon="['fas', 'long-arrow-alt-right']" />
+              <font-awesome-icon style="color: lightgray" :icon="['fas', 'long-arrow-alt-right']" />
             </div>
-            <div>Accessories</div>
+            <div><strong>Accessories</strong></div>
             <div>
-              <font-awesome-icon :icon="['fas', 'long-arrow-alt-right']" />
+              <font-awesome-icon style="color: lightgray" :icon="['fas', 'long-arrow-alt-right']" />
             </div>
-            <div>Drifter Jeans</div>
+            <div><strong>Drifter Jeans</strong></div>
           </div>
-          <div style="text-align: left">Brand</div>
+          <div class="mt-2" style="text-align: left"><strong>(Brand)</strong></div>
           <div style="text-align: left">
-            <h2>Drifter Jeans</h2>
+            <h2 class="mt-2"><strong>Drifter Jeans</strong></h2>
           </div>
           <div style="text-align: left">Colorway</div>
-          <div style="text-align: left">$39.00</div>
-          <div style="text-align: left">
+          <div style="text-align: left"><h3>$39.00</h3></div>
+          <div style="text-align: left; color: darkgray">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
@@ -43,43 +41,38 @@
             PageMaker including versions of Lorem Ipsum
           </div>
           <div class="card" style="border: 0">
-            <div
-              class="card-header row"
-              style="
-                padding-left: 0;
-                text-align: left;
-                background-color: transparent;
-                border: 0;
-              "
-            >
-              <div class="col-3">More Colorways:</div>
+            <div class="card-header row mt-4" style="padding-left: 0; text-align: left; background-color: transparent; border: 0;">
+              <div class="col-3"><strong>More Colorways:</strong></div>
               <div class="col-3">
-                <img src="../assets/men-category.jpg" style="width: 100%" />
+                <img type="button" src="../assets/men-category.jpg" style="width: 100%" />
               </div>
               <div class="col-3">
-                <img src="../assets/men-category.jpg" style="width: 100%" />
+                <img type="button" src="../assets/men-category.jpg" style="width: 100%" />
               </div>
               <div class="col-3">
-                <img src="../assets/men-category.jpg" style="width: 100%" />
+                <img type="button" src="../assets/men-category.jpg" style="width: 100%" />
               </div>
             </div>
-            <div class="row text-left">
-              <div class="col-2">Size:</div>
+            <div class="row text-left mt-4">
+              <div class="col-3"><strong>Size:</strong></div>
               <div class="col-8">
                 <div class="row">
-                  <div class="col-2">S</div>
-                  <div class="col-2">M</div>
-                  <div class="col-2">L</div>
-                  <div class="col-2">XL</div>
-                  <div class="col-2">XXL</div>
-                  <div class="col-2">XXXL</div>
+                  <div class="col-2">
+                    <button style="border: 1px solid black" type="button" class="btn btn-light">S</button>
+                  </div>
+                  <div class="col-2">
+                    <button style="border: 1px solid black" type="button" class="btn btn-light">M</button>
+                  </div>
+                  <div class="col-2">
+                    <button style="border: 1px solid black" type="button" class="btn btn-light">L</button>
+                  </div>
+                  <div class="col-2">
+                    <button style="border: 1px solid black" type="button" class="btn btn-light">XL</button>
+                  </div>
                 </div>
-                <div>Size Guide</div>
+                <div class="mt-4" style="text-decoration: underline" type="button">Size Guide</div>
                 <div class="row">
-                  <div
-                    class="btn mr-4 mt-1"
-                    style="background-color: lightgray; border-radius: 25px"
-                  >
+                  <div class="btn mr-4 mt-2" style="background-color: #f7f8f9; border-radius: 25px">
                     <div style="float: left; padding: 5px">
                       <font-awesome-icon
                         type="button"
@@ -88,26 +81,14 @@
                     </div>
                     <div style="float: left; padding: 5px">1</div>
                     <div style="float: left; padding: 5px">
-                      <font-awesome-icon
-                        type="button"
-                        :icon="['fas', 'plus']"
-                      />
+                      <font-awesome-icon type="button" :icon="['fas', 'plus']"/>
                     </div>
                   </div>
-                  <div class="mt-3 mr-2">Add to cart</div>
-                  <div class="mt-3 mr-2">
-                    <font-awesome-icon
-                      style="
-                        background-color: black;
-                        color: white;
-                        border-radius: 25px;
-                        padding: 5px;
-                      "
-                      :icon="['fas', 'long-arrow-alt-right']"
-                      size="2x"
-                    />
+                  <div class="mt-4 mr-4"><strong>Add to cart</strong></div>
+                  <div class="mt-3 mr-4" type="button">
+                    <font-awesome-icon :icon="['fas', 'arrow-circle-right']" size="2x"/>
                   </div>
-                  <div class="mt-3">
+                  <div class="mt-4" type="button">
                     <font-awesome-icon :icon="['far', 'heart']" />
                   </div>
                 </div>
@@ -115,12 +96,21 @@
             </div>
           </div>
           <div class="mt-2 text-left">
-            <div>SKU:</div>
-            <div>Categories"</div>
-            <div>Tags: jeans, men</div>
+            <div>
+              <div style="color: darkgray" class="pull-left mr-2">SKU:</div>
+              <div class="pull-left">1122</div>
+            </div>
+            <div class="clear-both">
+              <div style="color: darkgray" class="pull-left mr-2">Categories:</div>
+              <div class="pull-left">men</div>
+            </div>
+            <div class="clear-both">
+              <div style="color: darkgray" class="pull-left mr-2">Tags:</div>
+              <div class="pull-left">jeans, men</div>
+            </div>
           </div>
-          <div class="mt-2 text-left">
-            <div style="float: left" class="mr-2">Share</div>
+          <div class="mt-2 text-left clear-both">
+            <div style="float: left; color: darkgray" class="mr-2">Share</div>
             <font-awesome-icon
               class="mr-2 mt-1 pull-left"
               :icon="['fab', 'facebook-f']"
@@ -140,14 +130,11 @@
         </div>
         <div class="col-8 text-left">
           <div class="card" style="border: none">
-            <div
-              class="card-header row mb-2"
-              style="text-align: left; background-color: transparent; border: 0"
-            >
-              <div class="col-3" style="padding-left: 0">Description</div>
-              <div class="col-3">Specifications</div>
+            <div class="card-header row mb-2" style="text-align: left; background-color: transparent; border: 0">
+              <div type="button" @click="description_clicked" class="col-3" id="descrip" style=" padding-left: 0"><h5>Description</h5></div>
+              <div type="button" @click="specifications_clicked" class="col-3" id="specs" style="color: darkgray"><h5>Specifications</h5></div>
             </div>
-            <div style="clear: both">
+            <div v-show="description" style="clear: both; color: darkgray">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
@@ -159,11 +146,20 @@
               publishing software like Aldus PageMaker including versions of
               Lorem Ipsum
             </div>
+            <div v-show="specifications" style="clear: both; color: darkgray">
+              so the leap into electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum
+            </div>
           </div>
         </div>
       </div>
         <div class="card mt-3" style="border: 0">
-          <div class="text-left card-header" style="border: none; background-color: transparent">Related Products</div>
+          <div class="text-left card-header" style="border: none; background-color: transparent">
+            <h4>Related Products</h4>
+          </div>
           <div
             class="card-header row mb-2"
             style="background-color: transparent; border: 0"
@@ -304,6 +300,44 @@ export default {
     Navbar,
     Header,
     Footer
+  },
+  data () {
+    return {
+      description: true,
+      specifications: false
+    }
+  },
+  methods: {
+    description_clicked () {
+      var descrip = document.getElementById('descrip')
+      var specs = document.getElementById('specs')
+      if (this.description) {
+        this.description = false
+        this.specifications = true
+        descrip.style.color = 'black'
+        specs.style.color = 'darkgray'
+      } else if (this.specifications) {
+        this.description = true
+        this.specifications = false
+        descrip.style.color = 'darkgray'
+        specs.style.color = 'black'
+      }
+    },
+    specifications_clicked () {
+      var descrip = document.getElementById('descrip')
+      var specs = document.getElementById('specs')
+      if (this.description) {
+        this.description = false
+        this.specifications = true
+        descrip.style.color = 'black'
+        specs.style.color = 'darkgray'
+      } else if (this.specifications) {
+        this.description = true
+        this.specifications = false
+        descrip.style.color = 'darkgray'
+        specs.style.color = 'black'
+      }
+    }
   }
 }
 </script>
